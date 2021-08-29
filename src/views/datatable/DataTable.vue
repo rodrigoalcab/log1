7279189af7007bfbcc629ff8bdc2cd1ba6878a96
Rodrigo Cabral <rodrigoalcab@gmail.com>
2021-08-29 14:17:50 +0100

    font awesome adicionada e botões bootstrap criados para editar e remover

--- src/views/datatable/DataTable.vue
+++ src/views/datatable/DataTable.vue
@@ -2,16 +2,31 @@
     <v-app>
         <v-main>
             <v-container>
+                <v-card-title>
+                    Nutrition
+                    <v-spacer></v-spacer>
+                    <v-text-field
+                            v-model="search"
+                            append-icon="mdi-magnify"
+                            label="Search"
+                            single-line
+                            hide-details
+                    ></v-text-field>
+                </v-card-title>
                 <v-data-table
                         :headers="headers"
                         :items="posts"
-                        :items-per-page="2"
+                        :items-per-page="5"
                         :search="search"
                         class="elevation-1"
                 >
-<!--                    <template v-slot:item.actions="{ item }">-->
-<!--                        <v-icon small @click="deleteItem(item.id)">mdi-delete</v-icon>-->
-<!--                    </template>-->
+                    <template v-slot:item.actions="{item}">
+                        <div style="display: flex;">
+                            <b-button variant="warning"><i class="fas fa-edit"></i></b-button>
+                            <b-button variant="danger" class="ml-2"><i class="fas fa-trash"></i></b-button>
+                        </div>
+                    </template>
+
                 </v-data-table>
             </v-container>
         </v-main>
@@ -28,10 +43,16 @@
                     { text: "User Id", value: "userId" },
                     { text: "Id", value: "id" },
                     { text: "Title", value: "title" },
-                    { text: "Body", value: "body" }
+                    { text: "Body", value: "body" },
+                    { text: "", value: "actions", sortable: false },
                 ],
             }
         },
+        methods: {
+            deleteItem() {
+                alert('deleting item...')
+            }
+        }
     }
 </script>
 
